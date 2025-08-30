1a9ac4c79e3f3da07b9af5c1f251fe46cc6570ca
Chemsdev <chemscreative@gmail.com>
2025-08-30 23:28:45 +0200

    test deploy

--- lambda_function/tools.py
+++ lambda_function/tools.py
@@ -2,9 +2,53 @@
 import os
 import requests
 from datetime import datetime  
-# import hubspot
 from dotenv import load_dotenv
 import json
+import boto3
+
+from hubspot.crm.deals import SimplePublicObjectInputForCreate
+import hubspot
+
+# ------------------------>
+AWS_CONNEXION_CHEMS = [
+    "ACCESS_KEY_ID_CHEMS",   
+    "SECRET_ACCESS_KEY_CHEMS",
+    "REGION_CHEMS"      
+]
+# ------------------------>
+
+
+# ------------------------>
+# Fonction permettent de se connecter à AWS. 
+def connexion_aws(liste_connexion=AWS_CONNEXION_CHEMS):
+    try:
+        load_dotenv()
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id     = os.environ.get(liste_connexion[0]),
+            aws_secret_access_key = os.environ.get(liste_connexion[1]),
+            region_name           = os.environ.get(liste_connexion[2])
+        )
+        
+        message = f"Connexion AWS réussie (région : {os.environ.get(liste_connexion[2])})."
+        print(message)
+        
+        return {
+            "status"  : "success",
+            "message" : message,
+            "client"  : s3_client
+        }
+
+    except Exception as e:
+        error_message = f"Échec de la connexion AWS : {e}"
+        print(error_message)
+
+        return {
+            "status"  : "error",
+            "message" : error_message,
+            "client"  : None
+        }
+# ------------------------>
 
 
 load_dotenv()
@@ -12,12 +56,7 @@ headers = {
     "Authorization": f"Bearer {os.environ.get('ACCESS_TOKEN_HUBSPOT')}",
     "Content-Type": "application/json"
 }
-
-
-
-
 ACCESS_TOKEN_HUBSPOT = os.getenv("HUBSPOT_API_KEY")
-
 
 # ------------------------------------------------------------------------>
 
